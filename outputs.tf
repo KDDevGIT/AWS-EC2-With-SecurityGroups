fecba84a2d9590707d80b1e5a4cca25a32709452
Kyler Dana <kymasterbox@gmail.com>
2025-08-29 07:53:23 -0700

    Added HTTP URL Outputto outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,12 @@ output "instance_id" {
     value = aws_instance.web.id
 }
 
+# Public IP
 output "public_ip" {
     value = aws_instance.web.public_ip
+}
+
+# HTTP URL
+output "http_url" {
+    value = "http://${aws_instance.web.public_ip}"
 }
